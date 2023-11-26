99684de5073bebb052641687d9c9c254b87d7712
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-11-26 19:58:13 +0100

    enable 1667 fragment

--- src/model.rs
+++ src/model.rs
@@ -10,36 +10,41 @@ pub trait VolumeReference: Send + Sync {
     fn url_path_base(&self) -> String;
 }
 impl dyn VolumeReference {
-    pub const VOLUMES: [&'static dyn VolumeReference; 6] = [
+    pub const VOLUMES: [&'static dyn VolumeReference; 7] = [
         &FullVolumeReference::SCROLL1,
         &FullVolumeReference::SCROLL2,
         &FullVolumeReference::SCROLL332,
         &FullVolumeReference::SCROLL1667,
+        &FullVolumeReference::FRAGMENT_PHerc1667Cr01Fr03,
         &SurfaceVolumeReference::SEGMENT_20230827161847,
         &SurfaceVolumeReference::SEGMENT_20231005123335,
     ];
 }
 
 pub struct FullVolumeReference {
-    pub scroll_id: u16,
+    pub scroll_id: &'static str,
     pub volume: &'static str,
 }
 impl FullVolumeReference {
     pub const SCROLL1: FullVolumeReference = FullVolumeReference {
-        scroll_id: 1,
+        scroll_id: "1",
         volume: "20230205180739",
     };
     pub const SCROLL2: FullVolumeReference = FullVolumeReference {
-        scroll_id: 2,
+        scroll_id: "2",
         volume: "20230210143520",
     };
     pub const SCROLL332: FullVolumeReference = FullVolumeReference {
-        scroll_id: 332,
+        scroll_id: "332",
         volume: "20231027191953",
     };
     pub const SCROLL1667: FullVolumeReference = FullVolumeReference {
-        scroll_id: 1667,
+        scroll_id: "1667",
         volume: "20231107190228",
+    };
+    pub const FRAGMENT_PHerc1667Cr01Fr03: FullVolumeReference = FullVolumeReference {
+        scroll_id: "PHerc1667Cr01Fr03",
+        volume: "20231121133215",
     };
 }
 impl VolumeReference for FullVolumeReference {
