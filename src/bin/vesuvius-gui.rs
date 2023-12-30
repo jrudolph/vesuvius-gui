ad4c1161fd8f9fa2f893e8ea734b3a3a52a49e25
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-12-30 10:45:12 +0100

    restructure sources to prepare for multiple binaries

--- src/bin/vesuvius-gui.rs
+++ src/bin/vesuvius-gui.rs
@@ -1,5 +1,4 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use vesuvius_gui::gui::TemplateApp;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -14,12 +13,6 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "vesuvius-gui",
         native_options,
-        Box::new(|cc| {
-            Box::new(vesuvius_gui::TemplateApp::new(
-                cc,
-                std::env::args().nth(1),
-                std::env::args().nth(2),
-            ))
-        }),
+        Box::new(|cc| Box::new(TemplateApp::new(cc, std::env::args().nth(1), std::env::args().nth(2)))),
     )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-mod app;
 mod downloader;
+pub mod gui;
 mod model;
 mod volume;
-pub use app::TemplateApp;
