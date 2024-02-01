7c582c1252a211b65ea58fd7baea33a2f0cc11a9
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-02-01 17:52:40 +0100

    move to egui 0.25

--- src/bin/vesuvius-gui.rs
+++ src/bin/vesuvius-gui.rs
@@ -5,11 +5,7 @@ use vesuvius_gui::gui::TemplateApp;
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-    let native_options = eframe::NativeOptions {
-        initial_window_size: Some([400.0, 300.0].into()),
-        min_window_size: Some([300.0, 220.0].into()),
-        ..Default::default()
-    };
+    let native_options = Default::default();
     eframe::run_native(
         "vesuvius-gui",
         native_options,
