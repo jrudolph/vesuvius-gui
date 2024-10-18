693b03fc0a5772321a0ac19d1ac3c4fc324590f4
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-10-18 09:55:04 +0200

    include latest segment metadata

--- src/catalog/mod.rs
+++ src/catalog/mod.rs
@@ -93,10 +93,8 @@ impl Catalog {
 }
 
 pub fn load_segments() -> Vec<Segment> {
-    let zst_compressed = include_bytes!("../../vesuvius-segments-2024-09-26.json.zst");
+    let zst_compressed = include_bytes!("../../vesuvius-segments-2024-10-31.json.zst");
     let uncompressed = zstd_decompress(zst_compressed);
-
-    // interpret as utf8
     let json = String::from_utf8(uncompressed).unwrap();
     serde_json::from_str(&json).unwrap()
 }
