1659353ef68cc9a6e145dc47c56c02c70a4a09c2
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-12-27 14:15:21 +0100

    rendering in tiles

--- src/bin/vesuvius-render.rs
+++ src/bin/vesuvius-render.rs
@@ -217,6 +217,8 @@ impl Rendering {
         .tick_chars("→↘↓↙←↖↑↗");
 
         let tiles = self.uv_tiles();
+        let tiles_per_layer = tiles.len() as u64 / self.params.w_range.clone().count() as u64;
+        println!("Tiles per layer: {}", tiles_per_layer);
 
         let map_bar = ProgressBar::new(tiles.len() as u64)
             .with_style(count_style.clone())
@@ -233,10 +235,11 @@ impl Rendering {
         multi.add(download_bar.clone());
         download_bar.tick();
 
-        /* let render_bar = ProgressBar::new(tiles.len() as u64)
+        let render_bar = ProgressBar::new(tiles.len() as u64)
             .with_style(count_style.clone().tick_chars("▪▫▨▧▦▩"))
             .with_message("Rendering tiles");
-        multi.add(render_bar.clone()); */
+        multi.add(render_bar.clone());
+        render_bar.tick();
 
         let layers_bar = ProgressBar::new(self.params.w_range.clone().count() as u64)
             .with_style(count_style.tick_chars("⌷⌸⌹⌺"))
@@ -244,7 +247,7 @@ impl Rendering {
         multi.add(layers_bar.clone());
         layers_bar.tick();
 
-        let buf_size = 64;
+        let buf_size = 1024;
 
         stream::iter(tiles)
             .map(move |tile| {
@@ -267,30 +270,27 @@ impl Rendering {
                 }
             })
             .buffered(buf_size) // needs ordering because we deduplicate downloads here
-            /* .map(|tile| {
+            .map(|tile| {
+                let self_clone = self.clone();
                 let render_bar = render_bar.clone();
                 async move {
-                    self.render_tile(&tile).await;
+                    let tile_image = tokio::task::spawn_blocking(move || self_clone.render_tile(&tile).unwrap())
+                        .await
+                        .unwrap();
                     render_bar.inc(1);
-                    tile
+                    (tile, tile_image)
                 }
             })
-            .buffered(buf_size) */
-            .map(|tile @ UVTile { u, v, w }| {
+            .buffered(tiles_per_layer as usize)
+            .chunks(tiles_per_layer as usize)
+            .map(|tiles| {
                 let self_clone = self.clone();
                 let layers_bar = layers_bar.clone();
-                let tile_size = self.params.tile_size;
-                let is_last = u + tile_size > self.params.width && v + tile_size > self.params.height;
-
                 async move {
-                    let res =
-                        tokio::task::spawn_blocking(move || if is_last { self_clone.render_layer(w) } else { Ok(()) })
-                            .await
-                            .unwrap()
-                            .unwrap();
-                    if is_last {
-                        layers_bar.inc(1);
-                    }
+                    let res = tokio::task::spawn_blocking(move || self_clone.render_layer_from_tiles(tiles))
+                        .await
+                        .unwrap();
+                    layers_bar.inc(1);
                     res
                 }
             })
@@ -395,13 +395,37 @@ impl Rendering {
 
         Ok(())
     }
-    async fn render_tile(&self, uv_tile: &UVTile) -> Result<()> {
-        //std::thread::sleep(std::time::Duration::from_millis(1000));
-        Ok(())
-    }
-    fn render_layer(&self, w: usize) -> Result<()> {
+
+    fn render_layer_from_tiles(&self, tiles: Vec<(UVTile, Image)>) -> Result<()> {
         let width = self.params.width;
         let height = self.params.height;
+        let tile_size = self.params.tile_size;
+        let w = tiles[0].0.w;
+        let mut image = image::GrayImage::new(width as u32, height as u32);
+
+        // copy in all the tile images
+        for (UVTile { u, v, w }, tile_image) in tiles {
+            let tile_data = tile_image.data;
+            // blit into the right area of image
+            for lu in 0..tile_size {
+                for lv in 0..tile_size {
+                    let gu = u + lu;
+                    let gv = v + lv;
+                    if gu < width && gv < height {
+                        // edge tiles may spill over boundaries of target image
+                        image.put_pixel(gu as u32, gv as u32, Luma([tile_data[lv * tile_size + lu].r() as u8]));
+                    }
+                }
+            }
+        }
+
+        image.save(format!("{}/{:02}.png", self.params.target_dir, w)).unwrap();
+
+        Ok(())
+    }
+    fn render_tile(&self, UVTile { u, v, w }: &UVTile) -> Result<Image> {
+        let paint_width = self.params.tile_size;
+        let paint_height = self.params.tile_size;
 
         struct PanicDownloader {}
         impl Downloader for PanicDownloader {
@@ -417,46 +441,40 @@ impl Rendering {
             .unwrap()
             .to_string();
 
-        let v = volume::VolumeGrid64x4Mapped::from_data_dir(&dir, Box::new(PanicDownloader {}));
-        let world = Arc::new(RefCell::new(v));
+        let vol = volume::VolumeGrid64x4Mapped::from_data_dir(&dir, Box::new(PanicDownloader {}));
+        let world = Arc::new(RefCell::new(vol));
         let trilinear_interpolation = true;
         let base: Arc<RefCell<dyn VoxelPaintVolume + 'static>> = if trilinear_interpolation {
             Arc::new(RefCell::new(TrilinearInterpolatedVolume { base: world }))
         } else {
             world
         };
-        let obj = Arc::new(RefCell::new(ObjVolume::new(self.obj.clone(), base, width, height)));
+        let obj = Arc::new(RefCell::new(ObjVolume::new(
+            self.obj.clone(),
+            base,
+            self.params.width,
+            self.params.height,
+        )));
         let mut world = obj.borrow_mut();
-        //let mut buf = vec![0u8; width * height];
-        /* for v in 0..height {
-            if v % 500 == 0 {
-                println!("Layer z:{} v:{} / {}", w, v, height);
-            }
-            for u in 0..width {
-                let value = world.get([u as f64, v as f64, ((w - mid_w) as f64)], 1);
-                buf[v * width + u] = value;
-            }
-        } */
-        /* let image = image::GrayImage::from_raw(width as u32, height as u32, buf).unwrap();
-        image.save(format!("rescaled-layers/{:02}.png", w)).unwrap(); */
-        let mut image = Image::new(width, height);
+
+        let mut image = Image::new(paint_width, paint_height);
         world.paint(
-            [width as i32 / 2, height as i32 / 2, w as i32 - MID_W as i32],
+            [
+                *u as i32 + paint_width as i32 / 2,
+                *v as i32 + paint_height as i32 / 2,
+                *w as i32 - MID_W as i32,
+            ],
             0,
             1,
             2,
-            width,
-            height,
+            paint_width,
+            paint_height,
             1,
             1,
             &DrawingConfig::default(),
             &mut image,
         );
-        let data = image.data.iter().map(|c| c.r()).collect::<Vec<_>>();
-        let image = image::GrayImage::from_raw(width as u32, height as u32, data).unwrap();
-        image.save(format!("{}/{:02}.png", self.params.target_dir, w)).unwrap();
-
-        Ok(())
+        Ok(image)
     }
 }
 
@@ -495,8 +513,8 @@ async fn main_run() {
         obj_file: "/tmp/mesh_window_900414_950414_flatboi.obj".to_string(),
         width: 15312,
         height: 16097,
-        tile_size: 4096,
-        w_range: 25..=25,
+        tile_size: 1024,
+        w_range: 25..=41,
         target_dir: "/tmp".to_string(),
     };
 
@@ -506,17 +524,17 @@ async fn main_run() {
         obj_file: "/tmp/20231221180251.obj".to_string(),
         width: 12975,
         height: 9893,
-        tile_size: 4096,
-        w_range: 25..=41,
+        tile_size: 2048,
+        w_range: 0..=63,
         target_dir: "/tmp".to_string(),
     };
 
-    /* let params = RenderParams {
-        obj_file: "/home/johannes/tmp/pap/20230827161847.obj".to_string(),
+    /*let params = RenderParams {
+        obj_file: "/tmp/20230827161847.obj".to_string(),
         width: 5048,
         height: 9163,
-        tile_size: 4096,
-        w_range: 25..=41,
+        tile_size: 1024,
+        w_range: 0..=63,
         target_dir: "/tmp".to_string(),
     }; */
 
@@ -524,234 +542,7 @@ async fn main_run() {
     rendering.run(&multi).await.unwrap();
 }
 
-fn main3() {
-    const TILE_SERVER: &'static str = "https://vesuvius.virtual-void.net";
-
-    let obj_file = "/tmp/20231031143852.obj";
-    let width = 13577;
-    let height = 10620;
-
-    let w_range/* : std::ops::RangeInclusive<usize> */ = 25..=41; //25..=41; //32..=32; //15..=49;
-    let mid_w = 32 as i32;
-
-    let start = std::time::Instant::now();
-
-    // for better parallelism, tile the whole widthxheight into 16 tiles
-    let tiles: Vec<(usize, usize, usize)> = (0..width)
-        .step_by(width / 4)
-        .flat_map(|u| {
-            let cloned_range = w_range.clone();
-            (0..height)
-                .step_by(height / 4)
-                .flat_map(move |v| cloned_range.clone().into_iter().map(move |w| (u, v, w)))
-        })
-        .collect();
-    let tile_width = width / 4;
-    let tile_height = height / 4;
-
-    use indicatif::ParallelProgressIterator;
-    use rayon::prelude::*;
-
-    let obj_file = Arc::new(ObjVolume::load_obj(obj_file));
-
-    let style =
-        ProgressStyle::with_template("{spinner} {msg:30} {bar:80.cyan/blue} {pos}/{len} [{elapsed_precise}] ({eta}) ")
-            .unwrap()
-            .tick_chars("→↘↓↙←↖↑↗");
-    let requested_tiles = tiles
-        .into_par_iter()
-        .progress_with_style(style.clone())
-        .with_message("Mapping segment...")
-        .flat_map(|(u, v, w)| {
-            let dummy = Arc::new(RefCell::new(DummyVolume2::new()));
-            //let world = Arc::new(RefCell::new(dummy.clone()));
-            let trilinear_interpolation = true;
-            let base: Arc<RefCell<dyn VoxelPaintVolume + 'static>> = if trilinear_interpolation {
-                Arc::new(RefCell::new(TrilinearInterpolatedVolume { base: dummy.clone() }))
-            } else {
-                dummy.clone()
-            };
-            let world = Arc::new(RefCell::new(ObjVolume::new(obj_file.clone(), base, width, height)));
-            let mut world = world.borrow_mut();
-
-            let mut image = Image::new(width / 4, height / 4);
-            let xyz = [
-                u as i32 + tile_width as i32 / 2,
-                v as i32 + tile_height as i32 / 2,
-                w as i32 - mid_w,
-            ];
-            world.paint(
-                xyz,
-                0,
-                1,
-                2,
-                tile_width,
-                tile_height,
-                1,
-                1,
-                &DrawingConfig::default(),
-                &mut image,
-            );
-            let res = dummy.borrow().requested_tiles.clone();
-            //println!("Tile: {},{} [{:?}]-> {:?}", u, v, xyz, res.len());
-            res
-        })
-        .collect::<HashSet<_>>();
-
-    let end = start.elapsed();
-    println!("Time needed for finding out files to download: {:?}", end);
-
-    //let requested_tiles = requested_tiles.into_iter().collect::<HashSet<_>>();
-    //&dummy.requested_tiles.borrow();
-    println!("Total tiles: {}", requested_tiles.len());
-
-    let dir = BaseDirs::new().unwrap().cache_dir().join("vesuvius-gui");
-    // FIXME: proper base path missing
-
-    let requested_tiles = requested_tiles
-        .iter()
-        .filter(|(x, y, z)| {
-            let file_name = format!(
-                "{}/64-4/d01/z{:03}/xyz-{:03}-{:03}-{:03}-b255-d01.bin",
-                dir.to_str().unwrap(),
-                z,
-                x,
-                y,
-                z
-            );
-            !std::path::Path::new(&file_name).exists()
-        })
-        .collect::<Vec<_>>();
-
-    // total size = 64^3 * requested_tiles.len()
-    println!(
-        "Requested tiles: {} total download size: {} MB",
-        requested_tiles.len(),
-        requested_tiles.len() * 64 * 64 * 64 / 1024 / 1024
-    );
-
-    let (sender, receiver) = std::sync::mpsc::channel();
-    let mut downloader = SimpleDownloader::new(
-        dir.to_str().unwrap(),
-        TILE_SERVER,
-        &FullVolumeReference::SCROLL1,
-        None,
-        sender,
-        false,
-    );
-
-    // queue max 16 tiles to downloader and only schedule new when one is finished
-    let dstyle =
-    ProgressStyle::with_template("{spinner} {msg:30} {bar:80.cyan/blue} [{elapsed_precise}] ({eta}) {decimal_bytes}/{decimal_total_bytes} ({decimal_bytes_per_sec})")
-    .unwrap().tick_chars("→↘↓↙←↖↑↗");
-
-    let bar = ProgressBar::new(requested_tiles.len() as u64 * 64 * 64 * 64)
-        .with_style(dstyle)
-        .with_message("Downloading tiles");
-    let queue = requested_tiles.iter().cloned().collect::<Vec<_>>();
-    let mut iter = queue.iter();
-    let mut running = 0;
-    let mut finished = 0;
-    while finished < requested_tiles.len() {
-        while running < 32 {
-            if let Some((x, y, z)) = iter.next() {
-                let state = Arc::new(Mutex::new(DS::Queuing));
-                downloader.queue((
-                    state.clone(),
-                    *x,
-                    *y,
-                    *z,
-                    Quality {
-                        bit_mask: 255,
-                        downsampling_factor: 1,
-                    },
-                ));
-                running += 1;
-                //println!("[{},{}] Queued downloading tile: {},{},{}", running, finished, x, y, z);
-            } else {
-                /* println!(
-                    "No more tiles to download, running: {} finished: {}/{}",
-                    running,
-                    finished,
-                    requested_tiles.len()
-                ); */
-                std::thread::sleep(std::time::Duration::from_millis(1000));
-                break;
-            }
-        }
-        while running > 0 && receiver.try_recv().is_ok() {
-            finished += 1;
-            running -= 1;
-            /* println!(
-                "[{},{}] Finished downloading tile: {},{},{}",
-                running, finished, x, y, z
-            ); */
-
-            bar.inc(64 * 64 * 64);
-        }
-        std::thread::sleep(std::time::Duration::from_millis(10));
-    }
-    bar.finish();
-
-    println!("Finished downloading all tiles");
-
-    w_range
-        .into_iter()
-        .collect::<Vec<_>>()
-        .into_par_iter()
-        .progress_with_style(style.clone())
-        .with_message("Rendering layers")
-        .for_each(move |w| {
-            let (sender, _) = std::sync::mpsc::channel();
-            let downloader = SimpleDownloader::new(
-                dir.to_str().unwrap(),
-                TILE_SERVER,
-                &FullVolumeReference::SCROLL1,
-                None,
-                sender,
-                false,
-            );
-            let v = volume::VolumeGrid64x4Mapped::from_data_dir(dir.to_str().unwrap(), Box::new(downloader));
-            let world = Arc::new(RefCell::new(v));
-            let trilinear_interpolation = true;
-            let base: Arc<RefCell<dyn VoxelPaintVolume + 'static>> = if trilinear_interpolation {
-                Arc::new(RefCell::new(TrilinearInterpolatedVolume { base: world }))
-            } else {
-                world
-            };
-            let obj = Arc::new(RefCell::new(ObjVolume::new(obj_file.clone(), base, width, height)));
-            let mut world = obj.borrow_mut();
-            //let mut buf = vec![0u8; width * height];
-            /* for v in 0..height {
-                if v % 500 == 0 {
-                    println!("Layer z:{} v:{} / {}", w, v, height);
-                }
-                for u in 0..width {
-                    let value = world.get([u as f64, v as f64, ((w - mid_w) as f64)], 1);
-                    buf[v * width + u] = value;
-                }
-            } */
-            /* let image = image::GrayImage::from_raw(width as u32, height as u32, buf).unwrap();
-            image.save(format!("rescaled-layers/{:02}.png", w)).unwrap(); */
-            let mut image = Image::new(width, height);
-            world.paint(
-                [width as i32 / 2, height as i32 / 2, w as i32 - mid_w as i32],
-                0,
-                1,
-                2,
-                width,
-                height,
-                1,
-                1,
-                &DrawingConfig::default(),
-                &mut image,
-            );
-            let data = image.data.iter().map(|c| c.r()).collect::<Vec<_>>();
-            let image = image::GrayImage::from_raw(width as u32, height as u32, data).unwrap();
-            image.save(format!("test_layers/{:02}.png", w)).unwrap();
-        });
-}
-
+use image::Luma;
 use tokio::runtime::Handle;
 
 async fn monitor_runtime_stats(multi: &MultiProgress) {
